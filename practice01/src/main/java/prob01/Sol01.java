c23b07107cf7135501c7b21b0610773b3ea56b3b
h2-young <h2young5618@pusan.ac.kr>
2024-11-19 16:18:35 +0900

    init commit

---
+++ practice01/src/main/java/prob01/Sol01.java
@@ -0,0 +1,24 @@
+package prob01;
+
+import java.util.Scanner;
+
+public class Sol01 {
+	public static void main(String[] args) {
+		
+		/* 코드 작성 */
+		int number;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("수를 입력하세요 : ");
+		number = scanner.nextInt();
+		
+		if (number % 3 == 0 && number != 0) {
+			System.out.println("3의 배수입니다.");
+		} else {
+			System.out.println("3의 배수가 아닙니다.");
+		}
+		
+		scanner.close();
+	}
+}
